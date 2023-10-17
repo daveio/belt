08801f5b2148e86f77aa9796caf9d7ecad7ebb76
Dave Williams <dave@dave.io>
2023-10-17 17:47:31 +0100

    cleanup and retarget workflows

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod whois;
 
-// use stuff
 fn main() {
     whois::whois_toy_impl().expect("WHOIS toy implementation failed to run");
 }
--- src/whois.rs
+++ src/whois.rs
@@ -9,21 +9,24 @@ mod data;
 pub(crate) fn whois_toy_impl() -> Result<(), ParseError> {
     // TODO: async
     let domain_name = "dave.io";
-    // let whois = WhoIs::from_path("data/whois.json").expect("Failed to load whois.json");
-    // let server_name =
-    //     data::whois_server_for(domain_name.to_string()).expect("Failed to get server name");
-    // let whois = WhoIs::from_host(server_name.as_str()).expect("Failed to contact WHOIS server");
+
     let whois =
         WhoIs::from_string(&data::WHOIS_SERVERS_JSON).expect("Failed to contact WHOIS server");
+
     let whois_response = whois
         .lookup(WhoIsLookupOptions::from_string(domain_name).expect("Failed to parse domain name"))
         .expect("Failed to look up domain");
+
     let parsed_response = parse_info(domain_name, &whois_response);
+
     let expiry = DateTime::parse_from_rfc3339(&parsed_response.expiration_date)
         .expect("Failed to parse expiration date")
         .with_timezone(&Utc);
+
     let now = Utc::now();
+
     let date_diff = date_component::calculate(&now, &expiry);
+
     println!("{:?}", date_diff);
 
     Ok(())
