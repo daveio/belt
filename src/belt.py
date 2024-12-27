e58e369120856611b6018e98f2112dc0c7b41b5a
Dave Williams <dave@dave.io>
2024-12-27 19:24:42 +0000

    dns boilerplate

--- src/belt.py
+++ src/belt.py
@@ -2,6 +2,7 @@ import click
 
 from keypair import compose_keypair
 from audioinfo import compose_audioinfo
+from dns import dns_lookup
 from meta import version as belt_version
 
 
@@ -33,6 +34,18 @@ def keypair(print_script: bool) -> None:
 @click.argument('path', nargs=1, type=click.Path(), default=".")
 def audioinfo(path: click.Path) -> None:
     click.echo(compose_audioinfo(path))
+
+@cli.group()
+def dns() -> None:
+    pass
+
+@dns.command()
+@click.argument("query", nargs=1, type=str, required=True)
+@click.argument("record_type", nargs=1, type=str, required=False, default="A")
+@click.option("-s", "server", type=str, help="DNS server to use", default="1.1.1.1")
+@click.option("-r", "root", type=bool, help="Use root servers directly", default=False)
+def lookup(query: str, record_type: str, server: str, root: bool) -> None:
+    click.echo(dns_lookup(query, record_type, server, root))
 
 
 if __name__ == "__main__":
---
+++ src/dns.py
@@ -0,0 +1,6 @@
+def dns(arg: str) -> str:
+    pass
+
+
+def dns_lookup(query: str, record_type: str, server: str, root: bool) -> str:
+    return "DNS lookup result"
